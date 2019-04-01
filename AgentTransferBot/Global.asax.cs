81c52ccc07813ea8f6131bb205382a4d30e1abe8
hshahbaz <32860200+hshahbaz@users.noreply.github.com>
2019-04-01 11:37:29 -0700

    Update Global.asax.cs

--- AgentTransferBot/Global.asax.cs
+++ AgentTransferBot/Global.asax.cs
@@ -1,25 +1,48 @@
-﻿using Autofac;
-using Autofac.Integration.Mvc;
+using Autofac;
 using Autofac.Integration.WebApi;
-using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Web;
+using System.Reflection;
 using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Routing;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.WindowsAzure.Storage;
+using Autofac.Integration.Mvc;
 
 namespace AgentTransferBot
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
+         protected void Application_Start()
+            {
             RegisterBotDependencies();
 
             GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
+
+            var store = new InMemoryDataStore();
+
+            Conversation.UpdateContainer(
+                       builder =>
+                       {
+                           builder.Register(c => store)
+                                     .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                     .AsSelf()
+                                     .SingleInstance();
+
+                           builder.Register(c => new CachingBotDataStore(store,
+                                      CachingBotDataStoreConsistencyPolicy
+                                      .ETagBasedConsistency))
+                                      .As<IBotDataStore<BotData>>()
+                                      .AsSelf()
+                                      .InstancePerLifetimeScope();
+
+
+                       });
+            }
 
         private void RegisterBotDependencies()
         {
